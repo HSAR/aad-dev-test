95b94257fe39119bdd5bfe3b278b6753003e39c8
Shu Sam Chen <sschen@microsoft.com>
2016-11-15 14:51:25 +0000

    Updated client ID.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  exports.creds = {
  	returnURL: 'https://localhost:3000/auth/openid/return',
  	identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
- 	clientID: 'f3a1b3d0-3586-48b7-8844-aaacbf75c33f',
+ 	clientID: '8f78342d-3656-40b1-b8bb-47cbfecfb8ae',
  	clientSecret: 'GZcx4Dq+Qs7KaH+f1S8Sc4QyZ2HtgaFAf+jTgpHQEws=', // if you are doing code or id_token code
  	skipUserProfile: true, // for AzureAD should be set to true.
  	responseType: 'id_token code', // for login only flows use id_token. For accessing resources use `id_token code`
