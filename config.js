055a8f603dea566bbd645724db2bae9dd7a004be
Shu Sam Chen <sschen@microsoft.com>
2016-11-15 18:44:01 +0000

    Updated responseMode.

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ exports.creds = {
     clientSecret: 'Pd73ATiLoyocXMjvQ7ZKFjg', // if you are doing code or id_token code
     skipUserProfile: true, // for AzureAD should be set to true.
     responseType: 'id_token code', // for login only flows use id_token. For accessing resources use `id_token code`
-    responseMode: 'query' // For login only flows we should have token passed back to us in a POST
-    //scope: ['email', 'profile'] // additional scopes you may wish to pass
+    responseMode: 'form_post',
+    // Required if we use http for redirectUrl
+    allowHttpForRedirectUrl: true
 };
