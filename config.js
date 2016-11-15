3becf385cc89754ec334e903bb16c25801ab4e91
Shu Sam Chen <sschen@microsoft.com>
2016-11-15 14:33:21 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+  // Don't commit this file to your public repos. This config is for first-run
+  //
+ exports.creds = {
+ 	returnURL: 'https://localhost:3000/auth/openid/return',
+ 	identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
+ 	clientID: 'f3a1b3d0-3586-48b7-8844-aaacbf75c33f',
+ 	clientSecret: 'GZcx4Dq+Qs7KaH+f1S8Sc4QyZ2HtgaFAf+jTgpHQEws=', // if you are doing code or id_token code
+ 	skipUserProfile: true, // for AzureAD should be set to true.
+ 	responseType: 'id_token code', // for login only flows use id_token. For accessing resources use `id_token code`
+ 	responseMode: 'query', // For login only flows we should have token passed back to us in a POST
+ 	//scope: ['email', 'profile'] // additional scopes you may wish to pass
+ };
