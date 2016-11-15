99356e08c275919b5764d69d4a1e749c9cd9579a
Shu Sam Chen <sschen@microsoft.com>
2016-11-15 18:41:07 +0000

    Updated returnUri

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.creds = {
     // Required. It must be tenant-specific endpoint, common endpoint is not supported to use B2C
     // feature.
     identityMetadata: 'https://login.microsoftonline.com/aaddevtest.onmicrosoft.com/v2.0/.well-known/openid-configuration',
-    returnURL: 'https://aad-dev-test.herokuapp.com/auth/openid/return',
+    returnURL: 'https://aad-dev-test.herokuapp.com/auth/openid/return/',
     clientID: '510cab99-0c1c-4a9d-9e49-3c7bb707222b',
     clientSecret: 'Pd73ATiLoyocXMjvQ7ZKFjg', // if you are doing code or id_token code
     skipUserProfile: true, // for AzureAD should be set to true.
