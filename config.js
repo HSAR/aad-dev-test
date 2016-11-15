c73dd4af5c510558a8e1671246f81ae08ee04be2
Shu Sam Chen <sschen@microsoft.com>
2016-11-15 16:47:54 +0000

    Reverted to B2C application configuration.

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
  exports.creds = {
  	returnURL: 'https://aad-dev-test.herokuapp.com/auth/openid/return',
  	identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
- 	clientID: '17f074b0-8977-4fdf-9f18-f2ac51c41ed7',
- 	clientSecret: 'l66SE+4qrb7kDelCkO5qS5B0cYXRJkyslTZouPUZl+Y=', // if you are doing code or id_token code
+ 	clientID: 'f3a1b3d0-3586-48b7-8844-aaacbf75c33f',
+ 	clientSecret: '', // if you are doing code or id_token code
  	skipUserProfile: true, // for AzureAD should be set to true.
  	responseType: 'id_token code', // for login only flows use id_token. For accessing resources use `id_token code`
  	responseMode: 'query', // For login only flows we should have token passed back to us in a POST
