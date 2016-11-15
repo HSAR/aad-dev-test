31140d0a976fcf10e769dc7918e08a27910cf4a6
Shu Sam Chen <sschen@microsoft.com>
2016-11-15 16:23:09 +0000

    Changed returnURL to be the live URL.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
   // Don't commit this file to your public repos. This config is for first-run
   //
  exports.creds = {
- 	returnURL: 'https://localhost:3000/auth/openid/return',
+ 	returnURL: 'https://aad-dev-test.herokuapp.com/auth/openid/return',
  	identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
  	clientID: '8f78342d-3656-40b1-b8bb-47cbfecfb8ae',
  	clientSecret: 'GZcx4Dq+Qs7KaH+f1S8Sc4QyZ2HtgaFAf+jTgpHQEws=', // if you are doing code or id_token code
