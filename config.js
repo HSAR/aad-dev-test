62838666f9f5d0a4ffa5d31f70f4ad0b8de71427
Shu Sam Chen <sschen@microsoft.com>
2016-11-29 18:14:48 +0000

    Added comments on issuer to config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 // Don't commit this file to your public repos. This config is for first-run
 //
 exports.creds = {
-    // Required. It must be tenant-specific endpoint, common endpoint is not supported to use B2C
-    // feature.
+    // Required. It must be tenant-specific endpoint, common endpoint is not supported to use B2C feature.
     identityMetadata: 'https://login.microsoftonline.com/aaddevtest.onmicrosoft.com/v2.0/.well-known/openid-configuration',
     returnURL: 'https://aad-dev-test.herokuapp.com/auth/openid/return/',
     clientID: 'fa214e73-bed7-4367-a7f6-a3f69b1968b9',
@@ -17,5 +16,6 @@ exports.creds = {
     // Required to set to false if you don't want to validate issuer
     validateIssuer: true
     // Required if you want to provide the issuer(s) you want to validate instead of using the issuer from metadata
+    // issuer could be a string or an array of strings of the following form: 'https://sts.windows.net/<tenant_guid>/v2.0'
     //issuer: null
 };
